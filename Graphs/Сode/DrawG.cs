6431772eae001f5d880a61d0efcdff9911ba3e11
Булат Хабиров <bulyn2001@mail.ru>
2021-11-05 19:10:34 +0300

    1

--- Graphs/Сode/DrawG.cs
+++ Graphs/Сode/DrawG.cs
@@ -71,7 +71,9 @@ namespace Graphs
             {
                 gr.DrawLine(darkGoldPen, V1.x, V1.y, V2.x, V2.y);
                 point = new PointF((V1.x + V2.x) / 2, (V1.y + V2.y) / 2);
-                gr.DrawString(((char)('a' + numberE)).ToString(), fo, br, point);
+                string res = ((char)('a' + numberE)).ToString();
+                res += $"({E.weight})";
+                gr.DrawString(res, fo, br, point);
                 drawVertex(V1.x, V1.y, (E.v1 + 1).ToString());
                 drawVertex(V2.x, V2.y, (E.v2 + 1).ToString());
             }
--- Graphs/Сode/Edge.cs
+++ Graphs/Сode/Edge.cs
@@ -2,11 +2,12 @@
 {
     class Edge
     {
-        public int v1, v2;
-        public Edge(int v1, int v2)
+        public int v1, v2,weight;
+        public Edge(int v1, int v2,int weight=0)
         {
             this.v1 = v1;
             this.v2 = v2;
+            this.weight = weight;
         }
     }
 }
